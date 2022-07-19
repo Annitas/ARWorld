16776a819de0514cb0d4a0f5d9146315218ec8fd
Anita <deadfoxmaster15@gmail.com>
2022-07-19 20:38:16 +0600

    Yellow dots

---
+++ WorldTracking/WorldTracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTracking
+//
+//  Created by Anita Stashevskaya on 19.07.2022.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        //world origin is the starting position in the real world
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
